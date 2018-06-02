7e5c8d4a210c407bf68becd80c5964efa646ab27
Gerard Vivancos <gerard.vivancos@gmail.com>
2018-06-03 00:43:40 +0200

    Improve PersistingObject so it supports being a Singleton

--- Assets/Scripts/PersistingObject.cs
+++ Assets/Scripts/PersistingObject.cs
@@ -5,8 +5,30 @@ using UnityEngine.SceneManagement;
 
 public class PersistingObject : MonoBehaviour {
 
+    [Tooltip("If checked, the object will destroy itself if there's another PersistingObject with the same singletonID")]
+    [SerializeField] bool singleton = false;
+
+    [Tooltip("String to check for singleton compliance")]
+    [SerializeField] string singletonId = "";
+
 	// Use this for initialization
 	void Awake() {
-        GameObject.DontDestroyOnLoad(gameObject);
+        if (singleton && NumberOfObjectsLikeThisOne() > 1) {
+            GameObject.Destroy(gameObject);
+        } else {
+            GameObject.DontDestroyOnLoad(gameObject);
+        }
 	}	
+
+    private int NumberOfObjectsLikeThisOne() {
+        PersistingObject[] persistingObjects =  FindObjectsOfType<PersistingObject>();
+        int numberOfObjectsWithTheSameId = 0;
+        foreach (PersistingObject po in persistingObjects) {
+            if (po.singletonId == singletonId) {
+                numberOfObjectsWithTheSameId++;
+            }
+        }
+
+        return numberOfObjectsWithTheSameId;
+    }
 }
