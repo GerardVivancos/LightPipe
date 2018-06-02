e5b4a46efaf4d85700abe85a54f1863e368b7d7f
Gerard Vivancos <gerard.vivancos@gmail.com>
2018-06-02 23:18:36 +0200

    Add SceneController

---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour {
+
+    // Possibly called via string reference, be careful if you have to rename this
+    void ReloadCurrentScene () {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex);
+    }
+}
