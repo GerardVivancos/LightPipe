aa5d652187d0b665c0a7c9a521d42b6c667954f3
Gerard Vivancos <gerard.vivancos@gmail.com>
2018-06-04 23:33:37 +0200

    Enemies create their own colliders

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -1,8 +1,18 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour {
+
+    private void Start() {
+        AddNonTriggerBoxCollider();
+    }
+
+    private void AddNonTriggerBoxCollider() {
+        BoxCollider boxCollider = gameObject.AddComponent<BoxCollider>();
+        boxCollider.isTrigger = false;
+    }
 
     private void OnParticleCollision(GameObject other) {
         GameObject.Destroy(gameObject);
